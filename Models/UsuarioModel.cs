349cf548e240bfe57646261ad8fe3ffb84b11460
speed01test <edwin12sv@gmail.com>
2022-03-16 17:07:25 -0600

    Mejoras en el script

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Concurrent;
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace Practica1.Models{
+    public class UsuarioModel{
+        public string nombreUsuario {get; set;}
+        public string contraseñaUsuario {get; set;}
+        
+        public string apellidoUusuario {get; set;}
+    }
+}
