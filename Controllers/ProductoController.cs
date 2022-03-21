55efdddcd79b54d0af6410e3267f61964ea6fde9
speed01test <edwin12sv@gmail.com>
2022-03-21 09:38:44 -0600

    Mejoras al proyecto

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using Practica1.Models;
 using System.Text.RegularExpressions;
 using Practica1.Controllers;
+using Practica1.Repository;
 namespace Practica1.Controllers;
 
 public class Producto : Controller{
     [Route("/Product")]
     public IActionResult VerProductos()
     {
-
+        List<ProductoModel> producto = new List<ProductoModel>();
+        producto = Repository.Producto.obtenerProductos();
+        ViewBag.producto = producto;
+        Console.WriteLine(producto);
         return View("/Views/Product/Index.cshtml");
     }
 }
